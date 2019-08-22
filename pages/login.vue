cc6614e42c1cafebd11927793d144f313355a2fc
AndrewAlan <andrewalangm@gmail.com>
2019-08-22 16:38:35 -0500

    Create layout and style of login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,12 @@
 <template>
     <div class="my-container">
-        <h1 class="subtitle">THE LOGIN</h1>
+        <h1 class="title">TODO APP</h1>
+        <div class="container sm-container mt-4">
+                <input type="text" class="form-control" placeholder="User name">
+                <br>
+                <input type="password" class="form-control" placeholder="Password">
+                <br>
+                <button type="button" class="btn btn-outline-primary">LOG IN</button>
+        </div>
     </div>
 </template>
